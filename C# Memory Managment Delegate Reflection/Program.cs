73484febc6cc625ca5095eac0296e4be505c8b8d
mzehra213 <rashided@code.edu.az>
2025-12-02 23:53:40 +0400

    first commit

---
+++ C# Memory Managment Delegate Reflection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__Memory_Managment_Delegate_Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
