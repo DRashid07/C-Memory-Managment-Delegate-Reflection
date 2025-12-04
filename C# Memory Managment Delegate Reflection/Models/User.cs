db6439eec69f3eaf667ccd5eec4af6671c50c898
mzehra213 <rashided@code.edu.az>
2025-12-04 23:36:41 +0400

    New Update

--- C# Memory Managment Delegate Reflection/Models/User.cs
+++ C# Memory Managment Delegate Reflection/Models/User.cs
@@ -17,8 +17,22 @@
         {
             Name = name;
             Surname = surname;
-            Login = login;
-            Password = password;
+            if (login.Length<3 || login.Length>16)
+            {
+                throw new ArgumentException("Login length must be between 3 and 20 characters.");
+            }
+            else 
+            {                 
+                Login = login;
+            }
+            if (password.Length < 6 || password.Length > 16 && !password.Any(char.IsDigit) && !password.Any(char.IsUpper) && !password.Any(char.IsLower))
+            {
+                throw new ArgumentException("Password length must be between 6 and 20 characters.");
+            }
+            else
+            {
+                Password = password;
+            }
             UserType = userType;
         }
         public bool IsAdmin()
@@ -29,5 +43,17 @@
         {
             return $"Name: {Name}, Surname: {Surname}, Login: {Login}, UserType: {UserType}";
         }
+        public void UpdatePassword(string newPassword) //sil 
+        {
+            if (newPassword.Length < 6 || newPassword.Length > 16 && !newPassword.Any(char.IsDigit) && !newPassword.Any(char.IsUpper) && !newPassword.Any(char.IsLower))
+            {
+                throw new ArgumentException("Password length must be between 6 and 20 characters.");
+            }
+            else
+            {
+                Password = newPassword;
+            }
+        }
     }
+    
 }
