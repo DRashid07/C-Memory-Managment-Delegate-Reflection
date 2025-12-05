c43fbe910c8a2a65e66958479c55ad004b300c5e
mzehra213 <rashided@code.edu.az>
2025-12-05 21:48:01 +0400

    Big Update

---
+++ C# Memory Managment Delegate Reflection/Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace C__Memory_Managment_Delegate_Reflection.Models
+{
+    internal class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public List<string> Ingredients { get; set; }=new List<string>();
+    }
+}
--- C# Memory Managment Delegate Reflection/obj/Debug/net8.0/C# Memory Managment Delegate Reflection.AssemblyInfo.cs
+++ C# Memory Managment Delegate Reflection/obj/Debug/net8.0/C# Memory Managment Delegate Reflection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# Memory Managment Delegate Reflection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73484febc6cc625ca5095eac0296e4be505c8b8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db6439eec69f3eaf667ccd5eec4af6671c50c898")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# Memory Managment Delegate Reflection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# Memory Managment Delegate Reflection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
